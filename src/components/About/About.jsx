40c5567ff68fc7e01f4e4a30b5156e2ebeb723f2
Jonathan Jesus Hortman <99038380+JonathanHortman@users.noreply.github.com>
2022-06-28 03:17:20 -0300

    port

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -19,15 +19,24 @@ import token_3 from '../../Images/token_3.png'
 import token_4 from '../../Images/token_4.png'
 import token_5 from '../../Images/token_5.png'
 import token_6 from '../../Images/token_6.png'
+import Aos from "aos"
+import "aos/dist/aos.css"
+import { useEffect } from 'react'
+
 const About = () => {
+
+useEffect (()=> {
+Aos.init({duration: 500})
+},[])
+
   return (
     <div id='About' className={style.container}>
 
       <div className={style.bloque1}>
         <div className={style.subBloque1}>
 
-          <div className={style.softTitle}>Soft Skills</div>
-          <div className={style.tokenContainer}>
+          <div data-aos="zoom-out-right"  className={style.softTitle}>Soft Skills</div>
+          <div data-aos="zoom-out-right"  className={style.tokenContainer}>
             <div className={style.tokenywordContainer}>
               <img className={style.tokenImage} src={token_2} alt="skills" />
               <p>Team work</p>
@@ -50,37 +59,37 @@ const About = () => {
             </div>
           </div>
         </div>
-        <div className={style.subBloque2}>
-          <div className={style.softTitle}>Tech Skills</div>
-          <div className={style.row1}>
-            <img className={style.images} src={react} alt="image" />
-            <img className={style.images} src={html} alt="image" />
-            <img className={style.images} src={js} alt="image" />
-            <img className={style.images} src={css} alt="image" />
-            <img className={style.images} src={redux} alt="image" />
-            <img className={style.images} src={express} alt="image" />
-            <img className={style.images} src={node} alt="image" />
+        <div className={style.subBloque2} >
+          <div data-aos="fade-up" className={style.softTitle}>Tech Skills</div>
+          <div  data-aos="fade-up" className={style.row1}>
+            <img  className={style.images} src={react} alt="image" />
+            <img  className={style.images} src={html} alt="image" />
+            <img  className={style.images} src={js} alt="image" />
+            <img  className={style.images} src={css} alt="image" />
+            <img  className={style.images} src={redux} alt="image" />
+            <img  className={style.images} src={express} alt="image" />
+            <img  className={style.images} src={node} alt="image" />
           </div>
-          <div className={style.row2}>
-            <img className={style.images} src={git} alt="image" />
-            <img className={style.images} src={github} alt="image" />
-            <img className={style.images} src={postgresql} alt="image" />
-            <img className={style.images} src={ts} alt="image" />
-            <img className={style.images} src={sequelize} alt="image" />
-            <img className={style.images} src={linux} alt="image" />
+          <div data-aos="fade-up" className={style.row2} >
+            <img  className={style.images} src={git} alt="image" />
+            <img  className={style.images} src={github} alt="image" />
+            <img  className={style.images} src={postgresql} alt="image" />
+            <img  className={style.images} src={ts} alt="image" />
+            <img  className={style.images} src={sequelize} alt="image" />
+            <img  className={style.images} src={linux} alt="image" />
           </div>
         </div>
       </div>
 
-      <div className={style.bloque2}>
+      <div  className={style.bloque2}>
         <div>
-          <img className={style.huevo} src={token} alt="token image" />
-        </div>
-        <div className={style.text1}>who I am ?</div>
-        <div className={style.text2}>I'm a Full Stack Developer from Buenos Aires, Argentina.<br/>
+          <img data-aos="zoom-out-left" className={style.huevo} src={token} alt="token image" />
+        </div >
+        <div data-aos="fade-up" className={style.text1} >who I am ?</div>
+        <div data-aos="fade-up" className={style.text2} >I'm a Full Stack Developer from Buenos Aires, Argentina.<br/>
           I have serious passion for Frontend development, UI Design<br/> animations and dynamic user experiences. <br/>
          </div>
-         <a href="#Contact">
+         <a data-aos="fade-up" href="#Contact">
          <p className={style.special}>› Let's make something special ‹</p>
          </a>
       </div>
---
+++ src/components/IndividualProject/IndividualProject.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from './IndividualProject.module.css'
+
+const IndividualProject = () => {
+  return (
+    <div className={style.container}>
+        <img src="" alt="" />
+    </div>
+  )
+}
+
+export default IndividualProject
